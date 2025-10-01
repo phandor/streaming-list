3fc10b3088a81676948b01c01616c2931454e70f
lami <dorella.mphan@gmail.com>
2025-10-01 17:02:09 +0200

    Add media item view

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:streaming_list/domain/entities/movie.dart';
+
+import '../domain/entities/media_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,11 +9,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Streaming List'), centerTitle: true,),
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) => const Text("Hehe")
-      )
-    );
+        appBar: AppBar(
+          title: const Text('My Streaming List'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) => _MediaItemView(
+                mediaItem: Movie(
+                    name: "name", note: "note", timeStoppedInSeconds: 900))));
+  }
+}
+
+class _MediaItemView extends StatelessWidget {
+  const _MediaItemView({required this.mediaItem});
+
+  final MediaItem mediaItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
